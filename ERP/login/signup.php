99d7ad57ca85b4ad95d87dd727a2b10f313fd65e
Nickychan7 <nickychan.008@gmail.com>
2022-01-26 17:18:45 +0700

    IT-asset-management

---
+++ ERP/login/signup.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!------ Include the above in your HEAD tag ---------->
+
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<!------ Include the above in your HEAD tag ---------->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<section class="login-block">
+	    <div class="container">
+		<div class="row">
+			<div class="col-md-4 login-sec">
+			    <h2 class="text-center">Be Our New Member</h2>
+				<form class="login-form">
+				  <div class="form-group">
+				    <label for="exampleInputEmail1" class="text-uppercase">Full Name</label>
+				    <input type="text" class="form-control" placeholder="">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1" class="text-uppercase">Email</label>
+				    <input type="password" class="form-control" placeholder="">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1" class="text-uppercase">Username</label>
+				    <input type="text" class="form-control" placeholder="">		 
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1" class="text-uppercase">Password</label>
+				    <input type="password" class="form-control" placeholder="">
+				  </div>
+				</form>
+			</div>
+
+			<div class="col-md-4 login-sec">
+			    <h2 class="text-center">Company Profile</h2>
+				<form class="login-form">
+				  <div class="form-group">
+				    <label for="exampleInputEmail1" class="text-uppercase">Company Name</label>
+				    <input type="text" class="form-control" placeholder="">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1" class="text-uppercase">Company Address</label>
+				    <input type="text" class="form-control" placeholder="">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputEmail1" class="text-uppercase">Telephone</label>
+				    <input type="text" class="form-control" placeholder="">
+				  </div>
+	
+				  <!-- <div class="form-check"> -->
+				    <button type="submit" class="btn btn-login float-right">Submit</button>
+				  <!-- </div> -->
+				  
+				</form>
+				<div class="copy-text">Already a member?<a href="index.php"> Sign in now</a></div>
+			</div>
+
+			<div class="col-md-4 banner-sec">
+	            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	                 <ol class="carousel-indicators">
+	                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	                  </ol>
+	            <div class="carousel-inner" role="listbox">
+	    <div class="carousel-item active">
+	      <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+	      <div class="carousel-caption d-none d-md-block">
+	        <div class="banner-text">
+	            <h2>This is Heaven</h2>
+	            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+	        </div>	
+	  </div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
+	      <div class="carousel-caption d-none d-md-block">
+	        <div class="banner-text">
+	            <h2>This is Heaven</h2>
+	            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+	        </div>	
+	    </div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block img-fluid" src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg" alt="First slide">
+	      <div class="carousel-caption d-none d-md-block">
+	        <div class="banner-text">
+	            <h2>This is Heaven</h2>
+	            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+	        </div>	
+	    </div>
+	  </div>
+	            </div>	   
+			    
+			</div>
+		</div>
+	</div>
+	</section>
+</body>
+</html>
